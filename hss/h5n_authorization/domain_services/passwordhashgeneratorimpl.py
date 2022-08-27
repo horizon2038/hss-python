6fd4846ab0c698ec92a7f49a1b337f9955f2f8d1
horizon2038 <rekka728@gmail.com>
2022-08-27 11:41:18 +0900

    update
    .
    
    update PasswordHashGeneratorImpl

--- hss/h5n_authorization/domain_services/passwordhashgeneratorimpl.py
+++ hss/h5n_authorization/domain_services/passwordhashgeneratorimpl.py
@@ -12,6 +12,17 @@ class PasswordHashGeneratorImpl():
     def __init__(self):
         pass
 
+    def generate_hash(self, password: Password) -> HashedPassword:
+        salt: str = self.__generate_salt(password.get_password())
+        salt_added_password = password.get_password() + salt
+        hashed_password: str = self.__generate_sha256(salt_added_password)
+        return HashedPassword(hashed_password)
+
+    def __generate_salt(self, password: str) -> str:
+        md5_hashed_salt: str = self.__generate_md5(password)
+        salt: str = self.__extract_8word(md5_hashed_salt)
+        return salt
+
     def __generate_md5(self, password: str) -> str:
         md5_hashed_password: str = hashlib.md5(password.encode()).hexdigest()
         return md5_hashed_password
@@ -22,20 +33,3 @@ class PasswordHashGeneratorImpl():
 
     def __extract_8word(self, password: str) -> str:
         return password[0:7]
-
-    def __generate_salt(self, password: str) -> str:
-        md5_hashed_salt: str = self.__generate_md5(password)
-        salt: str = self.__extract_8word(md5_hashed_salt)
-        return salt
-
-    def generate_hash(self, password: Password) -> HashedPassword:
-        salt: str = self.__generate_salt(password.get_password())
-        salt_added_password = password.get_password() + salt
-        hashed_password: str = self.__generate_sha256(salt_added_password)
-        return HashedPassword(hashed_password)
-
-if __name__ == "__main__":
-    phg = PasswordHashGeneratorImpl()
-    password: Password = Password("Halcyon441")
-    hashed_password: HashedPassword = phg.generate_hash(password)
-    print(hashed_password.get_password())
