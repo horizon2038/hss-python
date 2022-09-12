366354fd1151e7c43ad98f0b7ba2709bb5f3a726
horizon2038 <rekka728@gmail.com>
2022-09-12 22:21:04 +0900

    remove
    .
    
    remove ip-address loader

--- hss/h5n_authorization/infrastructure/ipaddressloader.py
+++
@@ -1,5 +0,0 @@
-from typing import Protocol
-
-class IPAddressLoader(Protocol):
-    def load_ip_address() -> str:
-        pass
--- hss/h5n_authorization/infrastructure/ipaddressloaderimpl.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-class IPAddressLoaderImpl():
-    def __init__(self) -> None:
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    def load_ip_address(self) -> str:
-        __ip_address: str = socket.gethostbyname(socket.gethostname())
-        return __ip_address
